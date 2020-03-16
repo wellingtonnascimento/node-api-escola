49a51d105eab1eb04db7947760cb94fc43d2470d
Wellington Nascimento <wellingto_07@hotmail.com>
2020-03-16 09:07:42 -0300

    criando api rest

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port:
+  username:
+  password:
+  database:
+  define:{},
+  dialectOptions: {},
+  timezone: 'America/Sao_Paulo'
+};
