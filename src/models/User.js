b5a11dc3c96bd26c0d8d90080015d96927a247e6
Wellington Nascimento <wellingto_07@hotmail.com>
2020-03-17 11:00:52 -0300

    crud usuario para api

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 100],
+            msg: 'Campo nome deve ter entre 3 e 100 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email invalido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 20],
+            msg: 'A senha deve conter entre 6 a 20 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+    return this;
+  }
+}
