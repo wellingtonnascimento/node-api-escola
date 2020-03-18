b9e91cbd73b1ecc76f23b3192505c7070f0b6067
Wellington Nascimento <wellingto_07@hotmail.com>
2020-03-18 17:07:14 -0300

    tratando arquivos para que so possa enviar fotos

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,7 +1,20 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+
+const upload = multer(multerConfig).single('foto');
 
 class HomeController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
