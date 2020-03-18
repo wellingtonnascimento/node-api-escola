e47fb7de132b466cd365abf6f74e3fbe7c380eaf
Wellington Nascimento <wellingto_07@hotmail.com>
2020-03-18 11:18:56 -0300

    Middlewares de verificação do JWT

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 
 router.post('/', userController.create);
-router.get('/', userController.index);
+router.get('/', loginRequired, userController.index);
 router.get('/:id', userController.show);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
